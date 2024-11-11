5cb00686bdd6ec5634aec0434246761f3a69d435
Marshal Yordanos <marshalyordanosseifu@gmail.com>
2024-11-11 11:59:22 +0300

    chnages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import indicatorsService from "./indicators/IndicatorsService";
 import { indicators } from "../utils/indicators";
 import styled from "styled-components";
 import { Empty, Spin } from "antd";
-
+import { saveAs } from "file-saver";
+import * as XLSX from "xlsx";
 const Home = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [exelData, setExelData] = useState([]);
 
   useEffect(() => {
     searchReport();
@@ -19,41 +21,68 @@ const Home = () => {
         siteId,
         regionId
       );
-      console.log(" console.log(res.data);,", res[0]);
+      console.log(" console.log(res.data);,", res);
       const x = Object.keys(res[0]).map((key) => ({
         name: key,
         val: res[0][key],
       }));
-      console.log(x);
+      const y = x.map((data) => ({
+        name: indicators[data.name],
+        value: data.val,
+      }));
+      console.log("yyyyyyyyyyyyyy::", y);
       setData(x);
+      setExelData(y);
       setLoading(false);
     } catch (err) {
       setLoading(false);
       console.log(err);
     }
   }
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(exelData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+    saveAs(blob, `data-${new Date()}.xlsx`);
+  };
   return (
-    <div className="flex flex-wrap justify-center gap-12 max-w-[1200px] m-5 md:mx-auto">
-      {loading ? (
-        <SpinContainer className="spin_con flex justify-center items-center">
-          <Spin size="large" />
-        </SpinContainer>
-      ) : (
-        <>
-          {data.length == 0 ? (
-            <Empty />
-          ) : (
-            data.map((d) => <Card num={d?.val} title={d?.name} />)
-          )}
-        </>
-      )}
+    <div>
+      <div className="flex  justify-end mx-10 max-w-[1000px] m-5 md:mx-auto">
+        <button
+          onClick={() => exportToExcel()}
+          className="flex  bg-green-600 text-white py-2 px-10 rounded-full flex-wrap justify-center gap-12 "
+        >
+          Download Exel{" "}
+        </button>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-12 max-w-[1200px] m-5 md:mx-auto">
+        {loading ? (
+          <SpinContainer className="spin_con flex justify-center items-center">
+            <Spin size="large" />
+          </SpinContainer>
+        ) : (
+          <>
+            {data.length == 0 ? (
+              <Empty />
+            ) : (
+              data.map((d) => <Card num={d?.val} title={d?.name} />)
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
 const Card = ({ num, title }) => {
   return (
-    <div className="bg-gray-300 w-[300px] h-48 rounded-xl">
+    <div className="bg-blue-200 w-[300px] h-48 rounded-xl">
       <p className="text-5xl text-center pt-7">{num}</p>
       <p className="text-xl text-center pt-1 mx-1">{indicators[title]}</p>
     </div>
